9f6a4de2b6bfaa412a1dbbf31e62966e1cebc622
t100046uhn <108830888+t100046uhn@users.noreply.github.com>
2022-07-11 14:50:56 -0400

    Update jellyfin.sh

--- jellyfin.sh
+++ jellyfin.sh
@@ -7,3 +7,5 @@ curl -fsSL https://repo.jellyfin.org/ubuntu/jellyfin_team.gpg.key | sudo gpg --d
 echo "deb [arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release ) $( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release ) main" | sudo tee /etc/apt/sources.list.d/jellyfin.list
 sudo apt update
 sudo apt -y install jellyfin
+
+#username: 
