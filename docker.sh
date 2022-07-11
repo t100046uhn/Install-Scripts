f52ef9f5f06ef69031a0b5d3b9665f71f3d2840a
t100046uhn <108830888+t100046uhn@users.noreply.github.com>
2022-07-11 14:35:30 -0400

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,8 +1,26 @@
 #Installation of Docker
+#https://docs.docker.com/engine/install/debian/
+#Tested on Debian 11
+#!/bin/bash
 sudo apt-get update
 
- sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+#Setup Dependencies
+sudo apt-get install ca-certificates curl gnupg lsb-release
+
+#Add Docker’s official GPG key
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+#Repository Setup
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+#Docker Engine Installation
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+#Verify Docker Engine Installation with Commands Below
+#docker run hello-world
+#docker image ls -a
+#docker image rm
+#docker ps -a
+#docker rm 
