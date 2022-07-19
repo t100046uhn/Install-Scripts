66fa3670b60778584ac202080c3e476306b597cd
t100046uhn <108830888+t100046uhn@users.noreply.github.com>
2022-07-19 14:57:33 -0400

    Update unattended_setup.sh

--- unattended_setup.sh
+++ unattended_setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+
 #Installation of unattended-upgrades
 apt-get update && apt-get -y dist-upgrade
-apt-get install unattended-upgrades
+apt-get -y install unattended-upgrades
 dpkg-reconfigure --priority=low unattended-upgrades
